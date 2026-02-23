a19563666214b3533c2f4f0704c9a9d212832933
kushandagoat-ai <kushandagoat@gmail.com>
2026-02-22 17:42:06 -0800

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def factorial(x):
+    if x==0 or x==1:
+        return 1
+    else:   
+        return x*factorial(x-1)
+
+print("The factorial of 0:", factorial(0))
+print("The factorial of 1:",factorial(1))
+print("The factorial of 2:",factorial(2))
+print("The factorial of 3:",factorial(3))
+print("The factorial of 4:",factorial(4))
+print("The factorial of 5:",factorial(5))
+print("The factorial of 18:",factorial(18))
