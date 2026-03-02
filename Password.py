e07b0c54a8f4ca432449d5c5c7f2538fc9f05a09
kushandagoat-ai <kushandagoat@gmail.com>
2026-03-01 17:40:51 -0800

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,8 @@
+while True:
+    pswd = input("Enter which password you want")
+
+    if pswd == "Codingal123456":
+        print("Access granted for you!")
+        break
+    else:
+        print("Wrong passowrd. Try again")
