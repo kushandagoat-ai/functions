fb79eb677ff734cbb95b9bf0bcaec5a56eff8971
kushandagoat-ai <kushandagoat@gmail.com>
2026-03-01 17:34:00 -0800

    Add files via upload

---
+++ pass_activity.py
@@ -0,0 +1,13 @@
+for y in range(10):
+    if y % 20 ==0:
+        print("Twist")
+    
+    elif y % 15==0:
+        pass
+    elif y % 5 ==0:
+        print("fizz")
+    elif y % 3==0:
+        print("Bruzz")
+
+    else: 
+        print(y)
